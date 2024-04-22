8c4b197b021ef711973dd9a3e925cd5ac4f1ccf9
okorion <ok.or.orion@gmail.com>
2024-04-22 18:50:04 +0900

    📝: useProfile 초기화

--- src/constants/useProfile.ts
+++ src/constants/useProfile.ts
@@ -14,16 +14,16 @@ export type UserProfileProps = {
 
 export const USER_PROFILE: UserProfileProps = {
   title: 'Serengeti 개발자 ',
-  name: '마포빵돌이',
+  name: '축얌',
   education: '메이플대학 시그너스학과 소울마스터 졸업',
   career: `레벨 277 달성, 무릉 61층, 제네시스 무기 해방, 유니온 8800,
   검은마법사 격파, 더 시드, 50층 격파`,
   field: '시너지 딜러',
   techStack: ['react', 'javascript', 'typescript', 'html', 'css', 'aws', 'mongodb', 'nginx'],
   number: '010-1234-5678',
-  email: 'ok.or.orion@gmail.com',
+  email: 'serengti@serengti.net',
   notionPageId: '9110d2048ce0460084a576c0b09c012c',
-  githubLink: 'https://github.com/okorion',
+  githubLink: 'https://github.com/',
   aboutMe:
     '안녕하세요, 저는 Serengeti 개발자 축얌입니다..\n지금부터 제 소개를 시작해볼까 합니다.\n저에 대해 더 알고 싶으시다면\nScroll을 내려주세요.',
 };
